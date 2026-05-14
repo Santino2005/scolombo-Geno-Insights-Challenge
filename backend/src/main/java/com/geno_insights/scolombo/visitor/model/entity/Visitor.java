4016a7bbd4ac8ea159aef18245160cac7321ccd2
ElChurro274 <santinocolombo13@gmail.com>
2026-05-14 12:47:45 -0300

    Added the entities of the system

---
+++ backend/src/main/java/com/geno_insights/scolombo/visitor/model/entity/Visitor.java
@@ -0,0 +1,42 @@
+package com.geno_insights.scolombo.visitor.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "visitors")
+@Getter
+public class Visitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    @Setter
+    private String dni;
+
+    @Setter
+    @Column(nullable = false)
+    private String fullName;
+
+    @Setter
+    @Column(nullable = false)
+    private String company;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Sector sector;
+
+}
